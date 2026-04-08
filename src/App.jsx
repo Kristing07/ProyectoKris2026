b188aa7661dbea0496a142a307001efac782f2ad
KristingT <tabladakristing@gmail.com>
2026-04-08 08:47:08 -0600

    trabajo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Encabezado from "./components/navegacion/Encabezado";
+
+import Inicio from "./views/Inicio";
+import Categorias from "./views/Categorias";
+import Productos from "./views/Productos";
+import RutaProtegida from "./components/rutas/RutaProtegida";
+import Catalogo from "./views/Catalogo";
+import Pagina404 from "./views/Pagina404";
+import "./App.css";
+import Login from "./views/Login";
 
 const App = () => {
-
   return (
-    <>
-      <h1>Componente principal</h1>
-    </>
-  )
-}
+    <Router>
+      <Encabezado />
+      <main className="margen-superior-main">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<RutaProtegida><Inicio /></RutaProtegida>} />
+          <Route path="/categorias" element={<RutaProtegida><Categorias /></RutaProtegida>} />
+          <Route path="/catalogo" element={<Catalogo />} />
+          <Route path="/productos" element={<RutaProtegida><Productos /></RutaProtegida>} />
+          <Route path="*" element={<Pagina404 />} />
+        </Routes>
+      </main>
+    </Router>
+  );
+};
 
 export default App;
